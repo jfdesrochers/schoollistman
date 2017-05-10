6ca0fdb735c429cb3fe63e3119792a9718725388
Jean-Francois Desrochers <jfdesrochers@gmail.com>
2017-05-10 11:42:32 -0400

    Work on translator

--- js/utils/translator.js
+++ js/utils/translator.js
@@ -1,17 +1,22 @@
-setCookie = function(cname, cvalue, exdays) {
+const yaml = require('js-yaml')
+
+const setCookie = function(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     var expires = "expires=" + d.toUTCString();
     document.cookie = cname + "=" + cvalue + "; " + expires;
 };
 
+var currentLang = document.cookie.replace(/(?:(?:^|.*;\s*)currentlang\s*\=\s*([^;]*).*$)|^.*$/, "$1") || navigator.language || navigator.userLanguage || "en"
+currentLang = currentLang.substr(0, 2);
+
 const t = function (translateKey, translateToLang) {
-    const curLang = translateToLang || t._currentLang || document.cookie.replace(/(?:(?:^|.*;\s*)currentlang\s*\=\s*([^;]*).*$)|^.*$/, "$1") || navigator.language || navigator.userLanguage || "en";
-    curLang = curLang.substr(0, 2);
+    const curLang = translateToLang || currentLang
+    
 }
 
 t.setLang = function (newLang) {
-    t._currentLang = newLang
+    currentLang = newLang
 }
 
 module.exports = t
